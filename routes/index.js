264117fe74b28e8fe947621edb4b756f75057d2c
wk26 <wk26@fresnou.org>
2021-12-07 08:33:11 -0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+const pokemonController = require('../controllers/pokemonController');
+
+/* GET home page. */
+router.get('/', pokemonController.viewAll);
+
+module.exports = router;
